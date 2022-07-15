ed4805ffcb9985595222ff004076501b80771154
codeineslayer <87405300+codeineslayer@users.noreply.github.com>
2022-07-15 17:32:03 -0500

    Update TwoCardDraw.py
    added comments in

--- TwoCardDraw.py
+++ TwoCardDraw.py
@@ -1,29 +1,35 @@
+# Python program to play a two card draw game against the computer. Best hand: Pair of Aces.
 import random
 
+# Generate the list of cards based on the information below.
 cardlist = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-            "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "Ace": 14,}
+            "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "Ace": 14, }
 suits = {"Spades", "Clubs", "Diamonds", "Hearts"}
 color = {"Black", "Red"}
 cards = []
 for cardname in cardlist:
     for suit in suits:
-        cards.append({"Value": cardlist[cardname], "Card": cardname, "Suit": suit})
+        cards.append(
+            {"Value": cardlist[cardname], "Card": cardname, "Suit": suit})
 
-pccardnames = random.sample(cards, 2)
-pcsum = sum([c['Value'] for c in pccardnames])
+# Random select two cards for the user.
+usercards = random.sample(cards, 2)
+usersum = sum([c['Value'] for c in usercards])
 
-usercardnames = random.sample(cards, 2)
-usersum = sum([c['Value'] for c in usercardnames])
+# Random select two cards for the computer
+pccards = random.sample(cards, 2)
+pcsum = sum([c['Value'] for c in pccards])
 
+#Win condition, lose condition, overall logic.
 if usersum > pcsum:
     print("You win")
-    print("Your Cards: " + str(usercardnames))
-    print("PC Cards: " + str(pccardnames))
+    print("Your Cards: " + str(usercards))
+    print("PC Cards: " + str(pccards))
 elif pcsum > usersum:
     print("You lost!")
-    print("PC Cards: " + str(pccardnames))
-    print("Your Cards: " + str(usercardnames))
+    print("PC Cards: " + str(pccards))
+    print("Your Cards: " + str(usercards))
 elif pcsum == usersum:
     print("You tied!")
-    print("PC Cards: " + str(pccardnames))
-    print("Your Cards: " + str(usercardnames))
+    print("PC Cards: " + str(pccards))
+    print("Your Cards: " + str(usercards))

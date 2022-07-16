767cad1382389e34f70de90a2b7aef7da1551cd8
codeineslayer <marco.cervantez@gmail.com>
2022-07-15 19:07:21 -0500

    ff

--- TwoCardDraw.py
+++ TwoCardDraw.py
@@ -1,35 +1,89 @@
 # Python program to play a two card draw game against the computer. Best hand: Pair of Aces.
 import random
+from colorama import init, Fore, Style
+init(convert=True)
 
-# Generate the list of cards based on the information below.
-cardlist = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-            "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "Ace": 14, }
-suits = {"Spades", "Clubs", "Diamonds", "Hearts"}
-color = {"Black", "Red"}
-cards = []
-for cardname in cardlist:
-    for suit in suits:
-        cards.append(
-            {"Value": cardlist[cardname], "Card": cardname, "Suit": suit})
+SUIT_SIZE = 13
+DRAW_AMOUNT = 2
+SUITS = {
+        "spades": {
+          "display": Fore.BLACK + "♠" + Style.RESET_ALL,
+            },
+        "clubs": {
+            "display": Fore.BLACK + "♣ " + Style.RESET_ALL
+            },
+        "diamonds": {
+            "display": Fore.RED + "♦" + Style.RESET_ALL
+            },
+        "hearts": {
+            "display": Fore.RED + "♥ " + Style.RESET_ALL
+            }
+        }
+DECK = []
 
-# Random select two cards for the user.
-usercards = random.sample(cards, 2)
-usersum = sum([c['Value'] for c in usercards])
+class Card:
+    def __init__(self, number, suit):
+        self.number = number
+        self.suit = suit
 
-# Random select two cards for the computer
-pccards = random.sample(cards, 2)
-pcsum = sum([c['Value'] for c in pccards])
+    def value(self):
+        if self.number in range(2,10):
+            value = self.number
+        else:
+            value = 10
+        return int(value)
 
-#Win condition, lose condition, overall logic.
-if usersum > pcsum:
-    print("You win")
-    print("Your Cards: " + str(usercards))
-    print("PC Cards: " + str(pccards))
-elif pcsum > usersum:
-    print("You lost!")
-    print("PC Cards: " + str(pccards))
-    print("Your Cards: " + str(usercards))
-elif pcsum == usersum:
-    print("You tied!")
-    print("PC Cards: " + str(pccards))
-    print("Your Cards: " + str(usercards))
+    def print_format(self):
+        output = ""
+        if self.number == 11:  # Jack
+            output = "J"
+        elif self.number == 12:  # Queen
+            output = "Q"
+        elif self.number == 13:  # King
+            output = "K"
+        elif self.number == 1:  # Ace
+            output = "A"
+        else:
+            output = str(self.number)
+        return output
+
+    def print_to_screen(self):
+        return "%s%s" % (self.print_format(), SUITS[self.suit]['display'])
+
+    def __repr__(self):
+        return "%s-%s-%s" % (self.print_format(), self.suit, self.value())
+
+
+def populate_deck():
+    for suit in SUITS.keys():
+        for n in range(1, SUIT_SIZE+1):
+            card = Card(n, suit)
+            DECK.append(card)
+
+
+if __name__ == "__main__":
+    usercards = []
+    pccards = []
+
+    # Populate the DECK
+    populate_deck()
+    # shuffle DECK
+    random.shuffle(DECK)
+
+    for i in range(1, DRAW_AMOUNT+1):
+        usercards.append(DECK.pop())
+        pccards.append(DECK.pop())
+
+    usersum = sum([x.value() for x in usercards])
+    pcsum = sum([x.value() for x in pccards])
+
+    if usersum > pcsum:
+        outcome = "win"
+    elif usersum < pcsum:
+        outcome = "lose"
+    else:
+        outcome = "tie"
+
+    print("You %s!" % outcome)
+    print("Your hand: %s" % (', '.join([x.print_to_screen() for x in usercards])))
+    print("PC's hand: %s" %  (', '.join([x.print_to_screen() for x in pccards])))
